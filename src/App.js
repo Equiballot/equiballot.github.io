b4e12eef4ce2aab8677d0926dcf931993a47f36e
Cameron Yuen <cameronyuen@Cams-MacBook-Pro.local>
2023-08-07 21:40:42 -0400

    Added Google Analytics to the main App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home.js";
 import ViewCandidates from "./pages/ViewCandidates.js";
 import Profile from "./pages/Profile.js";
 import Stance from "./pages/Stance.js";
+import ReactGA from "react-ga";
+
+const G_ID = process.env.G_ID;
+
+ReactGA.initialize(G_ID);
 
 function App() {
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
+
     return (
         <HashRouter basename="/">
             <Routes>
